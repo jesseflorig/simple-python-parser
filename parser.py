c55c43b2c5d89e4414094ffb0ccc09ff79f777a2
Jesse Florig <jesseflorig@gmail.com>
2018-10-12 23:30:49 -0400

    added lab 4 notes

--- parser.py
+++ parser.py
@@ -1,12 +1,17 @@
+# 1. Import the operating system module
 import re
 
 regex_break_in = re.compile(r"POSSIBLE\sBREAK\-IN\sATTEMPT")
 break_in_count = 0
 
+# 2. Set the output file path
+# 3. Clear the contents of the output file
+# 4. Open the output file for appending
 with open("sample-data/auth.log", "r") as input_file:
     for line in input_file:
         if regex_break_in.search(line):
             break_in_count+=1
+            # 5. Append matched line to the output file
             print line      
 
 print 'Detected {count} attempted break-ins!'.format(count=break_in_count)
