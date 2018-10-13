65e39bb51ea3c2941de82c1fb8c8a3f437ad14d8
Jesse Florig <jesseflorig@gmail.com>
2018-10-12 23:17:56 -0400

    finished regex filtering

--- parser.py
+++ parser.py
@@ -1,7 +1,11 @@
 # 1. Import regex module
+import re
 
 # 2. Create regex pattern
+regex_break_in = re.compile(r"POSSIBLE\sBREAK\-IN\sATTEMPT")
+
 with open("sample-data/auth.log", "r") as input_file:
     for line in input_file:
         #3. Only print line if it matches regex pattern
-        print line      
+        if regex_break_in.search(line):
+            print line      
