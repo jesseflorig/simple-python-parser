5be3408b5ec7d0f635e1dde3d6f24da920b2f2f8
Jesse Florig <jesseflorig@gmail.com>
2018-10-12 23:04:01 -0400

    updated parser.py

--- parser.py
+++ parser.py
@@ -1,3 +1,6 @@
 # 1. Open a file for reading
-# 2. Loop through the lines in the file
-# 3. Print the lines to the command line
+with open("sample-data/auth.log", "r") as input_file:
+    # 2. Loop through the lines in the file
+    for line in input_file:
+        # 3. Print the lines to the command line
+        print line      
